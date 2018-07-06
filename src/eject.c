b37e49911ea9f51f24377444930bbb32b7742c58
Leandre <leandre.blanchard@epitech.eu>
2018-07-06 21:55:51 +0200

    eject file

---
+++ src/eject.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2018
+** cobra server
+** File description:
+** eject
+*/
+
+#include <stdio.h>
+
+#include "server.h"
+
+int _eject(cserver_t *server, const char *line)
+{
+	uint32_t addr;
+	cclient_t *ptr;
+	uint8_t *p = (uint8_t *)&addr;
+
+	if (server->clients->len == 0) {
+		trace(T_ERROR, "No client connected\n");
+		return (0);
+	}
+	addr = get_addr(line);
+	for (size_t i = 1; i < server->clients->len; ++i) {
+		ptr = server->clients->i[i];
+		if (*line == 'a' || addr == ptr->saddr->sin_addr.s_addr) {
+			dprintf(ptr->fd, "\a\aSTOP\a\a");
+			cserver_del_in_poll(server, ptr);
+			trace(T_INFO, "Ejecting %u.%u.%u.%u\n", p[0], p[1], p[2], p[3]);
+		}
+	}
+	return (0);
+}
